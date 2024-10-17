ae1428b3fd59be8913c7eec554f102061e09959e
MissedShot <missedshotik@gmail.com>
2024-10-17 22:55:24 +0300

    Added fonts group, added archive manager

--- plasma_minimal.sh
+++ plasma_minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # Basic Desktop
-dnf -y in plasma-desktop plasma-desktop-doc plasma-browser-integration plasma-nm plasma-nm-l2tp plasma-nm-openconnect plasma-nm-openswan plasma-nm-openvpn plasma-nm-pptp plasma-nm-vpnc sddm sddm-breeze sddm-kcm breeze-gtk colord-kde gnome-keyring-pam glibc-all-langpacks kde-gtk-config kde-settings-pulseaudio kdegraphics-thumbnailers kdeplasma-addons kdnssd kwalletmanager NetworkManager-config-connectivity-fedora systemd-oomd-defaults kinfocenter plymouth-system-theme plasma-vault kscreen
+dnf -y @"Fonts" in plasma-desktop plasma-desktop-doc plasma-browser-integration plasma-nm plasma-nm-l2tp plasma-nm-openconnect plasma-nm-openswan plasma-nm-openvpn plasma-nm-pptp plasma-nm-vpnc sddm sddm-breeze sddm-kcm breeze-gtk colord-kde gnome-keyring-pam glibc-all-langpacks kde-gtk-config kde-settings-pulseaudio kdegraphics-thumbnailers kdeplasma-addons kdnssd kwalletmanager NetworkManager-config-connectivity-fedora systemd-oomd-defaults kinfocenter plymouth-system-theme plasma-vault kscreen
 
 # Printer support
 # dnf in plasma-print-manager
@@ -10,7 +10,7 @@ dnf -y in plasma-desktop plasma-desktop-doc plasma-browser-integration plasma-nm
 # dnf in plasma-thunderbolt
 
 # Base KDE Apps
-dnf -y in plasma-systemmonitor dolphin konsole5 kate arc plasma-discover plasma-discover-notifier gwenview spectacle filelight
+dnf -y in plasma-systemmonitor dolphin ark konsole5 kate arc plasma-discover plasma-discover-notifier gwenview spectacle filelight
 
 # Other apps
 dnf -y in firefox vlc gnome-disk-utility
