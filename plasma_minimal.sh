e31fc3908c9fe3d15dfec5f6b6aeadcd381b8b6f
MissedShot <missedshotik@gmail.com>
2024-10-21 00:41:45 +0300

    File indexing fix

--- plasma_minimal.sh
+++ plasma_minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 # Basic Desktop
-dnf -y in plasma-desktop plasma-desktop-doc plasma-browser-integration plasma-nm plasma-nm-l2tp plasma-nm-openconnect plasma-nm-openswan plasma-nm-openvpn plasma-nm-pptp plasma-nm-vpnc sddm sddm-breeze sddm-kcm breeze-gtk colord-kde gnome-keyring-pam glibc-all-langpacks kde-gtk-config kde-settings-pulseaudio kdegraphics-thumbnailers kdeplasma-addons kdnssd kwalletmanager NetworkManager-config-connectivity-fedora systemd-oomd-defaults kinfocenter plymouth-system-theme plasma-vault kscreen pam-kwallet
+dnf -y in plasma-desktop plasma-desktop-doc plasma-browser-integration plasma-nm plasma-nm-l2tp plasma-nm-openconnect plasma-nm-openswan plasma-nm-openvpn plasma-nm-pptp plasma-nm-vpnc sddm sddm-breeze sddm-kcm breeze-gtk colord-kde gnome-keyring-pam glibc-all-langpacks kde-gtk-config kde-settings-pulseaudio kdegraphics-thumbnailers kdeplasma-addons kdnssd kwalletmanager NetworkManager-config-connectivity-fedora systemd-oomd-defaults kinfocenter plymouth-system-theme plasma-vault kscreen pam-kwallet kf6-baloo-file
 
 # Add multimedia codecs and fonts
 dnf -y group install Multimedia Fonts
@@ -13,7 +13,7 @@ dnf -y group install Multimedia Fonts
 # dnf in plasma-thunderbolt
 
 # Base KDE Apps
-dnf -y in plasma-systemmonitor dolphin ark konsole5 kate arc plasma-discover plasma-discover-notifier gwenview spectacle filelight
+dnf -y in plasma-systemmonitor dolphin ark konsole5 kate arc plasma-discover plasma-discover-notifier gwenview spectacle filelight kalk
 
 # Other apps
 dnf -y in firefox vlc gnome-disk-utility
